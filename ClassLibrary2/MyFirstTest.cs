152589845d5542372c213f56b70df188836a1bf5
bikerjake <jake.fulford@gmail.com>
2017-10-04 13:58:42 +0100

    Add project files.

---
+++ ClassLibrary2/MyFirstTest.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace ClassLibrary2
+{
+    public class MyFirstTest
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Test]
+        
+        public void myFirstTest()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Assert.AreEqual("Google", driver.Title);
+            IWebElement gBox = driver.FindElement(By.Name("q"));
+            gBox.SendKeys("MBA systems LTD");
+            gBox.Submit();
+
+            driver.Close();
+            driver.Quit();
+            Console.WriteLine("Test Completed");
+        }
+    }
+}
